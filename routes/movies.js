8770b609cb994e75f6dbbc718b502ac1cc32e534
Adam Marsala <adammarsala@hotmail.com>
2021-04-30 19:11:06 -0400

    Add axios call to ombd api and send response back to client

--- routes/movies.js
+++ routes/movies.js
@@ -1,11 +1,22 @@
-import axios from "axios";
 const express = require('express');
+const axios = require("axios");
 const router = express.Router();
+const omdbApiKey = process.env.API_KEY;
 
 router.post('/', function(req, res, next) {
-  console.log("HELLO: ", req.body);
-  const movie = req.body.movie
-  res.status(202).send(movie)
+
+  const movieQuery = req.body.movie;
+
+  axios.get(`http://www.omdbapi.com/?apikey=${omdbApiKey}&s=${movieQuery}&type=movie&page=2`)
+    .then(response => {
+      console.log("omdb api call successful with ", movieQuery);
+      res.status(202).send(response.data)
+    })
+    .catch(error => {
+      res.status(502).send(error)
+    })
+
+
 });
 
 module.exports = router;
