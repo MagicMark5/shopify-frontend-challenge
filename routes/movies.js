4946993bb6716f40dad8b4ac36937af528a37ab4
Adam Marsala <adammarsala@hotmail.com>
2021-05-05 16:04:39 -0400

    Add build from client run build to server, change port to 3000

--- routes/movies.js
+++ routes/movies.js
@@ -7,7 +7,7 @@ router.post('/', function(req, res, next) {
 
   const movieQuery = req.body.movie;
 
-  axios.get(`http://www.omdbapi.com/?apikey=${omdbApiKey}&s=${movieQuery}&type=movie&page=2`)
+  axios.get(`http://www.omdbapi.com/?apikey=${omdbApiKey}&s=${movieQuery}&type=movie&page=1`)
     .then(response => {
       console.log("omdb api call successful with ", movieQuery);
       res.status(202).send(response.data)
