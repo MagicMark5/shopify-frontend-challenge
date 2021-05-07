f35352201c9cac901a59ac3f6f2e286a8b829b3e
Adam Marsala <adammarsala@hotmail.com>
2021-05-07 15:45:22 -0400

    Remove unused modules, and re-organize dependencies, remove unused

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const db = require('./db');
-const dbHelpers = require('./helpers/dbHelpers')(db);
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const moviesRouter = require ('./routes/movies');
 
 const app = express();
@@ -15,11 +12,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/', indexRouter);
-//app.use('/api/users', usersRouter(dbHelpers));
 app.use('/api/movies', moviesRouter);
 
 app.get('*', (req, res) => {
