37cfc35ba4dbd411f5571aa8c82cc281a874b216
Adam Marsala <adammarsala@hotmail.com>
2021-05-09 14:59:59 -0400

    Delete unused JS code and users route file

--- client/src/styles/theShoppiesTitle.js
+++ client/src/styles/theShoppiesTitle.js
@@ -1,10 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    maxWidth: 752,
-  },
   background: {
     backgroundColor: "inherit"
   },
